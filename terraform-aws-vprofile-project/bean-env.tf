14db7cf37a26b10a9ee7e411fd9b3771997174a2
hkhcoder <137309866+hkhcoder@users.noreply.github.com>
2025-08-27 08:42:20 +0530

    Update bean-env.tf

--- terraform-aws-vprofile-project/bean-env.tf
+++ terraform-aws-vprofile-project/bean-env.tf
@@ -1,7 +1,7 @@
 resource "aws_elastic_beanstalk_environment" "vprofile-bean-prod" {
   name                = "vprofile-bean-prod"
   application         = aws_elastic_beanstalk_application.vprofile-prod.name
-  solution_stack_name = "64bit Amazon Linux 2023 v5.4.1 running Tomcat 10 Corretto 21"
+  solution_stack_name = "64bit Amazon Linux 2023 v5.7.4 running Tomcat 10 Corretto 21"
   cname_prefix        = "vprofile-bean-prod-pulsar"
 
   setting {
@@ -137,4 +137,5 @@ resource "aws_elastic_beanstalk_environment" "vprofile-bean-prod" {
   }
 
   depends_on = [aws_security_group.vprofile-bean-elb-sg, aws_security_group.vprofile-prodbean-sg]
+
 }
